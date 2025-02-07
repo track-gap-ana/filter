7071ad6ca1f5c8da8fbcba2ef01cc480c6d8fce0
Axel Ponten <axelpo@icecube.wisc.edu>
2025-02-07 17:35:55 +0100

    onlniefiltermask plots

---
+++ bin/filter-plots/example_run_plot_online_filter_mask.sh
@@ -0,0 +1,15 @@
+online_path="/data/user/axelpo/analysis-datasets/sig/compare_online_offline24_215984659/online_sig.hdf5"
+offline_path="/data/user/axelpo/analysis-datasets/sig/compare_online_offline24_215984659/offline24_sig.hdf5"
+
+setname="online_offline24_215984659_50m_mingap_ONLINEFILTERMASK"
+plotfolder="comparison_plots/"$setname"/"
+cutoff=0.0 # all filters
+filtermask="OnlineFilterMask"
+
+python plot_filter_plots.py \
+    --online $online_path \
+    --offline $offline_path \
+    --plot-folder $plotfolder \
+    --cutoff $cutoff \
+    --filtermask $filtermask \
+    
